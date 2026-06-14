05caae90ff61d86c5d99534dc9475182f8d91d1a
Zennovatus <6pfr56gm8p@privaterelay.appleid.com>
2026-06-14 22:47:44 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/StudyFlow/" // IMPORTANT: must match repo name
+  base: "/studyflow/"
 });
