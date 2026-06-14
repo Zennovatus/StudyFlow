0f5b0c8556054c44ce0691869b644f48d4e44d7b
Zennovatus <6pfr56gm8p@privaterelay.appleid.com>
2026-06-15 00:39:22 +0800

    Update CalendarView.jsx

--- src/views/CalendarView.jsx
+++ src/views/CalendarView.jsx
@@ -1,9 +1,21 @@
 import CalendarPanel from "../components/CalendarPanel";
 
-export default function CalendarView({ tasks, month, setMonth }) {
+export default function CalendarView({
+  tasks,
+  month,
+  setMonth,
+  onEditTask,
+  onDeleteTask
+}) {
   return (
     <div className="view-stack">
-      <CalendarPanel tasks={tasks} month={month} setMonth={setMonth} />
+      <CalendarPanel
+        tasks={tasks}
+        month={month}
+        setMonth={setMonth}
+        onEditTask={onEditTask}
+        onDeleteTask={onDeleteTask}
+      />
     </div>
   );
 }
